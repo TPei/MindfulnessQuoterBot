e9b99bd2de1a350a5c333838f418c062c066bc04
TPei <t.s.peikert@gmail.com>
2017-02-26 12:50:42 +0100

    streamline things a bit

--- main.rb
+++ main.rb
@@ -6,18 +6,13 @@ Dotenv.load
 
 token = ENV['BOT_TOKEN']
 
-while(true)
+loop do
   begin
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
-        if message.text
-          responses = []
-          chat_id = message.chat.id
-
-          responses.push CommandWatcher.parse(message.text)
-
-          mdp = MessageDispatcher.new(bot: bot, chat_id: chat_id)
-          mdp.dispatch_batch(responses)
+        if message&.text && message&.chat&.id
+          mdp = MessageDispatcher.new(bot: bot, chat_id: message.chat.id)
+          mdp.dispatch CommandWatcher.parse(message.text)
         end
       end
     end
